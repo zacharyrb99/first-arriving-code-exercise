d1aef2ed30420902309ce461dca4803e1353e967
zacharyrb99 <zach.boudreaux999@gmail.com>
2022-01-20 11:33:18 -0500

    set up Weather component, interval not working yet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Weather from "./Weather";  
 import './App.css';
 
 function App() {
-  const latitude = 37.6928;
-  const longitude = -77.4364;
   const [weather, setWeather] = useState({weather: null})
 
   const getWeatherData = async () => {
@@ -19,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1> First Arriving </h1>
+      <Weather data={weather} />
     </div>
   )
 }
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+const Weather = ({data}) => {
+    console.log(data);
+
+    return (
+        <div>
+            <h1>Weather</h1>
+        </div>
+    )
+}
+
+export default Weather;
