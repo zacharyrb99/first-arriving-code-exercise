9bae9d26a94387fbe413e8b538c2eaacfd40923f
zacharyrb99 <zach.boudreaux999@gmail.com>
2022-01-20 11:31:16 -0500

    set up requests to get weather data and save to state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const latitude = 37.6928;
+  const longitude = -77.4364;
+  const [weather, setWeather] = useState({weather: null})
+
+  const getWeatherData = async () => {
+    let weatherData = await axios.get("https://api.weather.gov/gridpoints/AKQ/44,83/forecast");
+    setWeather(weatherData);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(getWeatherData(), 10000);
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className="App">
       <h1> First Arriving </h1>
